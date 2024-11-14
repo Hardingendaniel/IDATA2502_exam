e5dd23c74bf1bed8ce2684996a1981eb29098299
Hardingendaniel <danieae@stud.ntnu.no>
2024-11-14 10:21:47 +0100

    Added a very simple react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to the portfolio assignment</h1>
+          <p>This is a simple React setup to use delivery pipeline for the portfolio assignment in IDATA2502</p>
+            <p>by Daniel Eikås</p>
+        </header>
+      </div>
+  );
+}
+
+export default App;
