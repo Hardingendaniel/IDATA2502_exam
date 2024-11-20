70284eaa2ad2d355dca83aea30402e93fe474de6
Hardingendaniel <danieae@stud.ntnu.no>
2024-11-20 20:27:31 +0100

    Try to deploy with github actions
    Managed to create a cluster through the command line now, with terraform

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ variable "tenant_id" {
 
 resource "azurerm_resource_group" "rg" {
   name     = "myResourceGroup"
-  location = "East US"
+  location = "Norway East"
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
@@ -50,7 +50,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "myregistry"
+  name                = "hardingenexam"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "Basic"
