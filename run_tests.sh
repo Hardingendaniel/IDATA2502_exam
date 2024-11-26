e2467ce3a87833f0dcb15ad7b641a0b08370550e
Hardingendaniel <danieae@stud.ntnu.no>
2024-11-26 21:00:19 +0100

    inshallah

--- run_tests.sh
+++ run_tests.sh
@@ -2,10 +2,11 @@
 # run_tests.sh
 
 # Wait for the deployed application to be ready
-sleep 10
+sleep 180
 
 kubectl wait --for=condition=ready pod -l app=my-app --timeout
 
 # Health check: Verify the application is up and running
 curl -f http://localhost:3000 || exit 1
+
 
