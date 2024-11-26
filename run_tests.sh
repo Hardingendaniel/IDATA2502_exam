31df1b3b19f7d52c21c73a65a21197d6dad16db8
Hardingendaniel <danieae@stud.ntnu.no>
2024-11-26 22:06:06 +0100

    Try to ping the service when deployed to kuber

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,12 @@
-##!/bin/sh
-## run_tests.sh
-#
-## Wait for the deployed application to be ready
-#sleep 10
-#
-#kubectl wait --for=condition=ready pod -l app=my-app --timeout
-#
-## Health check: Verify the application is up and running
-#curl -f http://localhost:3000 || exit 1
+#!/bin/sh
+# run_tests.sh
+
+# Wait for the deployed application to be ready
+sleep 10
+
+kubectl wait --for=condition=ready pod -l app=my-app --timeout
+
+# Health check: Verify the application is up and running
+curl -f http://localhost:3000 || exit 1
 
 
