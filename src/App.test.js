0c7dd49e41674cf8eadff50a179b3514d04aa236
Hardingendaniel <danieae@stud.ntnu.no>
2024-11-26 20:38:22 +0100

    added a dependency, and fixed a test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; // Ensures custom matchers are available
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the app without crashing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Welcome to the portfolio assignment/i)).toBeInTheDocument();
+  expect(screen.getByText(/This is a simple React setup to use delivery pipeline for the portfolio assignment in IDATA2502/i)).toBeInTheDocument();
+  expect(screen.getByText(/by Daniel Eikås/i)).toBeInTheDocument();
 });
+
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,11 +1,5 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import App from './App';
-
-test('renders the app without crashing', () => {
-  render(<App />);
-  expect(screen.getByText(/Welcome to the portfolio assignment/i)).toBeInTheDocument();
-  expect(screen.getByText(/This is a simple React setup to use delivery pipeline for the portfolio assignment in IDATA2502/i)).toBeInTheDocument();
-  expect(screen.getByText(/by Daniel Eikås/i)).toBeInTheDocument();
-});
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
